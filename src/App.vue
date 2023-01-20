03d7b7da3330be49e8f7c0f5e01ab78cd4f23d75
unknown <phyark@hotmail.com>
2023-01-20 12:06:02 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <img src="/vite.svg" class="logo" alt="Vite logo" />
+  </div>
+  <HelloWorld msg="In96.top" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
