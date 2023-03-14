12d3ebc617c7ff8181cdd80762ed88922501bce7
phyark <phyark@hotmail.com>
2023-03-15 00:44:48 +0800

    feat: add Baidu Statistics Code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>
-    <a href="/admin"><span class="i-text">I</span>n96</a>
+    <a href="https://github.com/phyark"><span class="i-text">I</span>n96</a>
   </h1>
+  <span class="fixRightBottom">phyark</span>
 </template>
 
 <style scoped>
@@ -17,8 +18,8 @@ a {
   color: #f1f1f1;
   position: relative;
   z-index: 1;
+  user-select: none;
 }
-
 h1::after {
   position: absolute;
   bottom: -20%;
@@ -32,14 +33,17 @@ h1::after {
   z-index: 0;
   transition: .3s ease .1s;
 }
+
 h1::after:hover {
   bottom: 12px;
   ;
 }
+
 .i-text {
   font-size: 0.8em;
   position: relative;
 }
+
 .i-text::before {
   position: absolute;
   display: inline-block;
@@ -52,27 +56,39 @@ h1::after:hover {
   transform: rotate(80deg);
   animation: myFrame 1s linear infinite;
 }
+.fixRightBottom {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  font-size: 2em;
+  font-family: fantasy;
+}
+
 @keyframes myFrame {
   0% {
     transform: rotate(80deg);
     width: 0.4em;
     height: 0.29em;
   }
+
   25% {
     transform: rotate(90deg);
     width: 0.4em;
     height: 0.26em;
   }
+
   50% {
     transform: rotate(92deg);
     width: 0.4em;
     height: 0.20em;
   }
+
   75% {
     transform: rotate(78deg);
     width: 0.4em;
     height: 0.26em;
   }
+
   75% {
     transform: rotate(80deg);
     width: 0.4em;
